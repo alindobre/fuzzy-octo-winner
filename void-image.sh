5002776cd7b801763570e2e7a59161b45518519a
Alin Dobre <alin.dobre@outlook.com>
2017-04-01 21:01:43 +0100

    Install xbps only if it's not present on the system
    Signed-off-by: Alin Dobre <alin.dobre@outlook.com>

--- void-image.sh
+++ void-image.sh
@@ -4,13 +4,18 @@ set -e
 
 apt update
 apt install -y git gcc make pkg-config libz-dev libssl-dev libarchive-dev
-XBPS_SRC=`mktemp -d /tmp/xbps.XXX`
-git clone https://github.com/voidlinux/xbps.git $XBPS_SRC
-cd $XBPS_SRC
-./configure --prefix=/
-make
-make install
+if ! type xbps-install 2>/dev/null; then
+  XBPS_SRC=`mktemp -d /tmp/xbps.XXX`
+  git clone https://github.com/voidlinux/xbps.git $XBPS_SRC
+  pushd $XBPS_SRC
+  ./configure --prefix=/
+  make
+  make install
+  popd
+  rm -rf $XBPS_SRC
+fi
 
 VOID_IMG=`mktemp -d /tmp/void.XXX`
 xbps-install -y -S -R http://repo3.voidlinux.eu/current -r $VOID_IMG base-voidstrap
 
+echo Image is available at $VOID_IMG
