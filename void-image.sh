efc77abc5ba73565f88d8a21b323c88f8f67a562
Alin Dobre <alin.dobre@outlook.com>
2017-04-01 22:27:27 +0100

    Fix bug where the piped xbps-install wasn't installing the repository key
    Signed-off-by: Alin Dobre <alin.dobre@outlook.com>

--- void-image.sh
+++ void-image.sh
@@ -4,9 +4,9 @@ set -e
 
 apt update
 apt install -y git gcc make pkg-config zlib1g-dev libssl-dev libarchive-dev
+XBPS_SRC=`mktemp -d /tmp/xbps.XXX`
+git clone https://github.com/voidlinux/xbps.git $XBPS_SRC
 if ! type xbps-install 2>/dev/null; then
-  XBPS_SRC=`mktemp -d /tmp/xbps.XXX`
-  git clone https://github.com/voidlinux/xbps.git $XBPS_SRC
   pushd $XBPS_SRC
   ./configure --prefix=/
   make
@@ -16,6 +16,8 @@ if ! type xbps-install 2>/dev/null; then
 fi
 
 VOID_IMG=`mktemp -d /tmp/void.XXX`
+mkdir -p $VOID_IMG/var/db/xbps/keys
+cp -v $XBPS_SRC/data/*.plist $VOID_IMG/var/db/xbps/keys
 xbps-install -y -S -R http://repo3.voidlinux.eu/current -r $VOID_IMG base-voidstrap
 
 echo Image is available at $VOID_IMG
