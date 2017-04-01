8024cb0a06b4a4bec5a47958a0099884bd6f1aa5
Alin Dobre <alin.dobre@outlook.com>
2017-04-01 20:52:49 +0100

    Initial version of the void image bootstrap script
    Signed-off-by: Alin Dobre <alin.dobre@outlook.com>

---
+++ void-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+apt install -y git gcc make pkg-config libz-dev libssl-dev libarchive-dev
+XBPS_SRC=`mktemp -d /tmp/xbps.XXX`
+git clone https://github.com/voidlinux/xbps.git $XBPS_SRC
+cd $XBPS_SRC
+./configure --prefix=/
+make
+make install
+
+VOID_IMG=`mktemp -d /tmp/void.XXX`
+xbps-install -S -R http://repo3.voidlinux.eu/current -r $VOID_IMG base-voidstrap
+
