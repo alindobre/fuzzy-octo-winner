5f42e7df315df9dcec5320c13ed2f0109ff70f5c
Alin Dobre <alin.dobre@outlook.com>
2018-03-12 21:28:13 +0000

    Fix python module always returning changed
    Signed-off-by: Alin Dobre <alin.dobre@outlook.com>

--- ansible/library/delete_test_dbs.py
+++ ansible/library/delete_test_dbs.py
@@ -8,21 +8,18 @@ def main():
   db=MySQLdb.connect(passwd=module.params["rootpw"], db="mysql")
   c=db.cursor()
   c.execute("select * from db where db like 'test%' and user='';")
-  changed=False
-  if c.rowcount > 0:
-    changed=True
+  changed=(c.rowcount > 0)
   if module.check_mode:
     c.close();
     module.exit_json(changed=changed)
   if c.rowcount > 0:
     c.execute("delete from db where db like 'test%' and user='';")
     if c.rowcount > 0:
-      changed=False
       module.fail_json(changed=False, msg='Failed to remove test databases')
     c.execute("flush privileges;")
     db.commit();
   c.close();
-  module.exit_json(changed=True, msg='test databases removed')
+  module.exit_json(changed=changed, msg='test databases removed')
 
 if __name__ == '__main__':
   main()
